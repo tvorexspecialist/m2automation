magento/magento2

Fix UK mobile number validation
The validation for UK mobile numbers is too restrictive on the third
digit, my own phone number begins 074 and the current validator does
not return a valid match, this loosens the restriction.

lib/web/mage/validation.js
@@ -320,7 +320,7 @@
         "mobileUK": [
             function (phone_number, element) {
                 return this.optional(element) || phone_number.length > 9 &&
-                    phone_number.match(/^((0|\+44)7(5|6|7|8|9){1}\d{2}\s?\d{6})$/);
+                    phone_number.match(/^((0|\+44)7\d{3}\s?\d{6})$/);
             },
             $.mage.__('Please specify a valid mobile number')
         ],

tvorexspecialist/m2automation

lib/web/mage/validation.js
@@ -384,7 +384,7 @@
         'mobileUK': [
             function (phoneNumber, element) {
                 return this.optional(element) || phoneNumber.length > 9 &&
-                    phoneNumber.match(/^((0|\+44)7(5|6|7|8|9){1}\d{2}\s?\d{6})$/);
+                    phoneNumber.match(/^((0|\+44)7\d{3}\s?\d{6})$/);
             },
             $.mage.__('Please specify a valid mobile number')
         ],