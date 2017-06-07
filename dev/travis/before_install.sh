magento/magento2

commit e68d69c00de96afc3c87a7188fb22391d8f30b16

Grunt static added to Traivs CI tests

.travis.yml
@@ -39,7 +39,4 @@ matrix:
 before_install: ./dev/travis/before_install.sh
 install: composer install --no-interaction --prefer-dist
 before_script: ./dev/travis/before_script.sh
-script:
-  - test $TEST_SUITE = "static" && TEST_FILTER='--filter "Magento\\Test\\Php\\LiveCodeTest"' || true
-  - if [ $TEST_SUITE != "js" ]; then phpunit -c dev/tests/$TEST_SUITE $TEST_FILTER; fi
-  - if [ $TEST_SUITE == "js" ]; then grunt spec; fi
+script: ./dev/travis/script.sh

dev/travis/before_install.sh
@@ -19,3 +19,17 @@ phpenv rehash;
 
 # If env var is present, configure support for 3rd party builds which include private dependencies
 test -n "$GITHUB_TOKEN" && composer config github-oauth.github.com "$GITHUB_TOKEN" || true
+
+# Node.js setup via NVM
+if [ $TEST_SUITE = "static" ] || [ test $TEST_SUITE == "js" ]; then
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+    nvm install $NODE_JS_VERSION
+    nvm use $NODE_JS_VERSION
+    node --version
+
+    npm install -g yarn
+    yarn global add grunt-cli
+fi

dev/travis/before_script.sh
@@ -75,19 +75,14 @@ case $TEST_SUITE in
         cat "$changed_files_ce" | sed 's/^/  + including /'
 
         cd ../../..
-        ;;
-    js)
-        curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-        export NVM_DIR="$HOME/.nvm"
-        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-        nvm install $NODE_JS_VERSION
-        nvm use $NODE_JS_VERSION
-        node --version
 
         cp package.json.sample package.json
         cp Gruntfile.js.sample Gruntfile.js
-        npm install -g yarn
-        yarn global add grunt-cli
+        yarn
+        ;;
+    js)
+        cp package.json.sample package.json
+        cp Gruntfile.js.sample Gruntfile.js
         yarn
 
         echo "Installing Magento"

dev/travis/script.sh
@@ -1,0 +1,10 @@
+case $TEST_SUITE in
+    static)
+        TEST_FILTER='--filter "Magento\\Test\\Php\\LiveCodeTest"' || true
+        phpunit -c dev/tests/$TEST_SUITE $TEST_FILTER
+        grunt static
+        ;;
+    js)
+        grunt spec;
+        ;;
+esac

tvorexspecialist/m2automation

commit 3c8fbe51f41d21bee30368728526296d12cee1ec

Fixed issue causing static test failure to report success/green on Travis
Regression was introduced in e68d69c00de96afc3; can't use a script that does not exit on command failure or exit code is always zero for anything but the last command the script ran. Additionally, moved the test script back into .travis.yml because test failure inside a .sh script will not include sufficient error output.

.travis.yml
@@ -22,11 +22,12 @@ env:
     - MAGENTO_HOST_NAME="magento2.travis"
   matrix:
     - TEST_SUITE=unit
+    - TEST_SUITE=static
+    - TEST_SUITE=js GRUNT_COMMAND=spec
+    - TEST_SUITE=js GRUNT_COMMAND=static
     - TEST_SUITE=integration INTEGRATION_INDEX=1
     - TEST_SUITE=integration INTEGRATION_INDEX=2
     - TEST_SUITE=integration INTEGRATION_INDEX=3
-    - TEST_SUITE=static
-    - TEST_SUITE=js
     - TEST_SUITE=functional ACCEPTANCE_INDEX=1
     - TEST_SUITE=functional ACCEPTANCE_INDEX=2
 matrix:
@@ -34,7 +35,9 @@ matrix:
     - php: 7.0
       env: TEST_SUITE=static
     - php: 7.0
-      env: TEST_SUITE=js
+      env: TEST_SUITE=js GRUNT_COMMAND=spec
+    - php: 7.0
+      env: TEST_SUITE=js GRUNT_COMMAND=static
     - php: 7.0
       env: TEST_SUITE=functional ACCEPTANCE_INDEX=1
     - php: 7.0
@@ -49,4 +52,11 @@ cache:
 before_install: ./dev/travis/before_install.sh
 install: composer install --no-interaction --prefer-dist
 before_script: ./dev/travis/before_script.sh
-script: ./dev/travis/script.sh
+script:
+  # Set arguments for variants of phpunit based tests; '|| true' prevents failing script when leading test fails
+  - test $TEST_SUITE = "static" && TEST_FILTER='--filter "Magento\\Test\\Php\\LiveCodeTest"' || true
+  - test $TEST_SUITE = "functional" && TEST_FILTER='dev/tests/functional/testsuites/Magento/Mtf/TestSuite/InjectableTests.php' || true
+
+  # The scripts for grunt/phpunit type tests
+  - if [ $TEST_SUITE != "js" ]; then phpunit -c dev/tests/$TEST_SUITE $TEST_FILTER; fi
+  - if [ $TEST_SUITE == "js" ]; then grunt $GRUNT_COMMAND; fi

dev/travis/before_install.sh
@@ -21,7 +21,7 @@ phpenv rehash;
 test -n "$GITHUB_TOKEN" && composer config github-oauth.github.com "$GITHUB_TOKEN" || true
 
 # Node.js setup via NVM
-if [ $TEST_SUITE = "static" ] || [ test $TEST_SUITE == "js" ]; then
+if [ test $TEST_SUITE == "js" ]; then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm

dev/travis/before_script.sh
@@ -75,22 +75,26 @@ case $TEST_SUITE in
         cat "$changed_files_ce" | sed 's/^/  + including /'
 
         cd ../../..
-
-        cp package.json.sample package.json
-        cp Gruntfile.js.sample Gruntfile.js
-        yarn
         ;;
     js)
         cp package.json.sample package.json
         cp Gruntfile.js.sample Gruntfile.js
         yarn
 
-        echo "Installing Magento"
-        mysql -uroot -e 'CREATE DATABASE magento2;'
-        php bin/magento setup:install -q --admin-user="admin" --admin-password="123123q" --admin-email="admin@example.com" --admin-firstname="John" --admin-lastname="Doe"
+        if [[ $GRUNT_COMMAND != "static" ]]; then
+            echo "Installing Magento"
+            mysql -uroot -e 'CREATE DATABASE magento2;'
+            php bin/magento setup:install -q \
+                --admin-user="admin" \
+                --admin-password="123123q" \
+                --admin-email="admin@example.com" \
+                --admin-firstname="John" \
+                --admin-lastname="Doe"
 
-        echo "Deploying Static Content"
-        php bin/magento setup:static-content:deploy -f -q -j=2 --no-css --no-less --no-images --no-fonts --no-misc --no-html-minify
+            echo "Deploying Static Content"
+            php bin/magento setup:static-content:deploy -f -q -j=2 \
+                --no-css --no-less --no-images --no-fonts --no-misc --no-html-minify
+        fi
         ;;
     functional)
         echo "Installing Magento"
@@ -117,7 +121,9 @@ case $TEST_SUITE in
 
         composer install && composer require se/selenium-server-standalone:2.53.1
         export DISPLAY=:1.0
-        sh ./vendor/se/selenium-server-standalone/bin/selenium-server-standalone -port 4444 -host 127.0.0.1 -Dwebdriver.firefox.bin=$(which firefox) -trustAllSSLCertificate &> ~/selenium.log &
+        sh ./vendor/se/selenium-server-standalone/bin/selenium-server-standalone -port 4444 -host 127.0.0.1 \
+            -Dwebdriver.firefox.bin=$(which firefox) -trustAllSSLCertificate &> ~/selenium.log &
+
         cp ./phpunit.xml.dist ./phpunit.xml
         sed -e "s?127.0.0.1?${MAGENTO_HOST_NAME}?g" --in-place ./phpunit.xml
         sed -e "s?basic?travis_acceptance_${ACCEPTANCE_INDEX}?g" --in-place ./phpunit.xml

dev/travis/script.sh
@@ -1,22 +1,0 @@
-#!/usr/bin/env bash
-
-# Copyright © Magento, Inc. All rights reserved.
-# See COPYING.txt for license details.
-
-case $TEST_SUITE in
-    static)
-        TEST_FILTER='--filter "Magento\\Test\\Php\\LiveCodeTest"' || true
-        phpunit -c dev/tests/$TEST_SUITE $TEST_FILTER
-        grunt static
-        ;;
-    js)
-        grunt spec
-        ;;
-    functional)
-        cd dev/tests/functional
-        vendor/phpunit/phpunit/phpunit -c phpunit.xml --debug testsuites/Magento/Mtf/TestSuite/InjectableTests.php
-        ;;
-    *)
-        phpunit -c dev/tests/$TEST_SUITE $TEST_FILTER;
-        ;;
-esac